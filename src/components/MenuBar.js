532b97175e827e8f370faf42d915778174a895cf
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-01-11 13:39:21 +0000

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const MenuBar = (props) => {
 
+
+
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -13,26 +15,29 @@ const MenuBar = (props) => {
 
   */
 
+
+
   return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
+    <div className="ui four item menu" >
+      <a className="item active" id="profile" onClick={props.handleShow} >
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className="item" id="photo" onClick={props.handleShow}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className="item" id="cocktail" onClick={props.handleShow}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className="item" id="pokemon" onClick={props.handleShow}>
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
+
+
 
 export default MenuBar
